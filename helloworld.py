df32f984d402f7059f29065937f2eb0873e91555
Qi Zhang <qizhang@hainanu.edu.cn>
2022-09-25 17:50:28 +0800

    effective version

--- helloworld.py
+++ helloworld.py
@@ -1,11 +1,14 @@
 import gym
-env = gym.make('CartPole-v1')
-env.reset()
-for _ in range(1000):
+from gym.utils.env_checker import check_env
 
+env = gym.make('CartPole-v1', render_mode="human")
+env.unwrapped
+env.reset()
+check_env(env, skip_render_check=False)
+for _ in range(1000):
     env.render()
 
-    env.step(env.action_space.sample()) # take a random action
+    env.step(env.action_space.sample())  # take a random action
 env.close()
 
 print('hello world')
